404b783a34bfad64ae59cf967dbfeefca0c0d323
Dennis Schneidermann <dsschneidermann@users.noreply.github.com>
2018-05-11 20:47:32 +0200

    Update to Hugo v0.38.1

--- Build/install-hugo-windows.bat
+++ Build/install-hugo-windows.bat
@@ -5,5 +5,4 @@ cd ..
 echo | set /p="Working directory: "
 cd
 powershell -Command "Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')"
-scoop update; scoop install ".Build\hugo-0.31.1.json"
-pause
+scoop update; scoop install "Build\hugo-0.38.1.json"; pause
